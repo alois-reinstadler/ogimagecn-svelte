09aa27a1701c0cb7f22940d58c29003c83030c5d
Alois R. <alois.reinstadler@proton.me>
2026-08-24 23:48:23 +0000

    fix: render image nodes as void elements

--- src/lib/components/OgNode.svelte
+++ src/lib/components/OgNode.svelte
@@ -14,4 +14,8 @@
 </script>
 
 <!-- Keep this block compact: literal SSR whitespace becomes Satori text nodes. -->
-<svelte:element this={node.tag} {...node.attrs} style={serializeStyle(node.style, node.tag)}>{#each node.children ?? [] as child}{#if typeof child === 'string'}{child}{:else}<OgNodeRenderer node={child} root={false} />{/if}{/each}{#if root && overlay}<div style="display:flex;position:absolute;inset:0px">{@render overlay()}</div>{/if}</svelte:element>
+{#if node.tag === 'img'}
+  <svelte:element this={'img'} {...node.attrs} style={serializeStyle(node.style, node.tag)} />
+{:else}
+  <svelte:element this={node.tag} {...node.attrs} style={serializeStyle(node.style, node.tag)}>{#each node.children ?? [] as child}{#if typeof child === 'string'}{child}{:else}<OgNodeRenderer node={child} root={false} />{/if}{/each}{#if root && overlay}<div style="display:flex;position:absolute;inset:0px">{@render overlay()}</div>{/if}</svelte:element>
+{/if}
